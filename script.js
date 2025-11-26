9a12a51235f82b75de28098d6122e8e6d85b2092
Nathan-0001 <nathan.hartshorn24@gmail.com>
2025-11-26 10:33:18 +0000

    Relocated calculator

--- script.js
+++ script.js
@@ -42,3 +42,12 @@ document.getElementById("backspace").onclick = function() {
     let screen = document.getElementById("screen");
     screen.value = screen.value.slice(0, -1);
 };
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("equals").click();
+    }
+});
